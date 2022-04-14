75050a80868ca05508887761448f77f1122afc2f
Matt <mqunell@gmail.com>
2022-04-13 20:26:40 -0700

    replace anonymous function with named function

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
-
 /**
  * All HTTP request methods (ie. GET, POST) go to this function. Use req.method
  * to see which was used and, if necessary, perform request-specific tasks.
@@ -9,6 +7,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
  * req is http.IncomingMessage plus some pre-built middlewares
  * res is http.ServerResponse plus some helper functions
  */
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	res.status(200).json({ text: 'Hello via API' });
-};
+}
