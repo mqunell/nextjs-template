443f2bd098b75195c56f4d37a8dcb12925b03297
Matt <mqunell@pm.me>
2023-06-03 13:35:20 -0700

    replace axios with fetch

--- src/app/demos/client-side-rendering/ClientSideRendering.tsx
+++ src/app/demos/client-side-rendering/ClientSideRendering.tsx
@@ -1,16 +1,15 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const ClientSideRendering = () => {
 	const [usernames, setUsernames] = useState<String>('Loading...');
 
 	useEffect(() => {
-		axios
-			.get('/api/users')
-			.then((res) => {
-				const names = res.data.map((user: User) => user.name);
+		fetch('/api/users')
+			.then((res) => res.json())
+			.then((data) => {
+				const names = data.map((user: User) => user.name);
 				setUsernames(names.join(', '));
 			})
 			.catch((error) => setUsernames(`Failed to load: ${JSON.stringify(error)}`));
@@ -19,7 +18,7 @@ const ClientSideRendering = () => {
 	return (
 		<>
 			<h1 className="text-lg underline">Client-side Rendering</h1>
-			<p>Use Axios, useEffect, and useState to retrieve data from `/api/users`:</p>
+			<p>Use fetch, useEffect, and useState to retrieve data from `/api/users`:</p>
 			<p>{usernames}</p>
 		</>
 	);
