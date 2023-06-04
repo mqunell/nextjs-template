620617f30c7167232508fb8c838ceee36500c691
Matt <mqunell@pm.me>
2023-06-04 16:09:28 -0700

    comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	content: [
 		'./src/app/**/*.{js,ts,jsx,tsx}',
-		'./src/pages/**/*.{js,ts,jsx,tsx}', // todo: delete this after migration
+		'./src/pages/**/*.{js,ts,jsx,tsx}', // Backwards compatibility with Pages Router
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
