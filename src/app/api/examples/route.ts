356b6a218a8c28b3c24862fb51b34703a1956f02
Matt <mqunell@pm.me>
2023-06-10 11:53:39 -0700

    add example for search params

--- src/app/api/examples/route.ts
+++ src/app/api/examples/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from 'next/server';
 
+export const GET = async (req: Request) => {
+	const { searchParams } = new URL(req.url);
+	const name = searchParams.get('name');
+
+	return NextResponse.json(name);
+};
+
 export const POST = async (req: Request) => {
 	const body = await req.json();
 	const { name } = body;
