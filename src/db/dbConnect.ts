94572fffb5e51e87b18fa624f2922366cce84091
Matt <mqunell@proton.me>
2025-08-29 13:55:17 -0700

    cleanup

--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -1,15 +1,15 @@
 import { Pool } from 'pg'
 
-const { POSTGRES_USER, POSTGRES_PASS, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB } = process.env
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASS } = process.env
 
-if (!POSTGRES_USER || !POSTGRES_PASS || !POSTGRES_HOST || !POSTGRES_PORT || !POSTGRES_DB) {
+if (!POSTGRES_HOST || !POSTGRES_PORT || !POSTGRES_DB || !POSTGRES_USER || !POSTGRES_PASS) {
 	throw new Error('Postgres config missing in .env')
 }
 
 export const pool = new Pool({
+	host: POSTGRES_HOST,
+	database: POSTGRES_DB,
 	user: POSTGRES_USER,
 	password: POSTGRES_PASS,
-	host: POSTGRES_HOST,
-	port: Number(POSTGRES_PORT),
-	database: POSTGRES_DB,
+	ssl: {}, // Neon only
 })
